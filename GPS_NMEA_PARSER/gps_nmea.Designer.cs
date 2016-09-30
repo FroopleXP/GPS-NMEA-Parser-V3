c09af7fcf913276b0b1ba8df321ca8dc6c59152d
FroopleXP <connor@frooplexp.com>
2016-09-30 18:21:51 +0100

    Started the app, just started the design...

---
+++ GPS_NMEA_PARSER/gps_nmea.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GPS_NMEA_PARSER {
+    partial class gps_nmea {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gps_nmea));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gps_device_comb = new System.Windows.Forms.ComboBox();
+            this.gps_baud_comb = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.gps_device_comb);
+            this.groupBox1.Controls.Add(this.gps_baud_comb);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(200, 148);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect to GPS device";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select GPS Device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Baud rate";
+            // 
+            // gps_device_comb
+            // 
+            this.gps_device_comb.FormattingEnabled = true;
+            this.gps_device_comb.Location = new System.Drawing.Point(13, 39);
+            this.gps_device_comb.Name = "gps_device_comb";
+            this.gps_device_comb.Size = new System.Drawing.Size(174, 21);
+            this.gps_device_comb.TabIndex = 1;
+            // 
+            // gps_baud_comb
+            // 
+            this.gps_baud_comb.FormattingEnabled = true;
+            this.gps_baud_comb.Location = new System.Drawing.Point(13, 84);
+            this.gps_baud_comb.Name = "gps_baud_comb";
+            this.gps_baud_comb.Size = new System.Drawing.Size(174, 21);
+            this.gps_baud_comb.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect to GPS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GPS NMEA Parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox gps_device_comb;
+        private System.Windows.Forms.ComboBox gps_baud_comb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- GPS_NMEA_PARSER/gps_nmea.cs
+++ GPS_NMEA_PARSER/gps_nmea.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace GPS_NMEA_PARSER {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class gps_nmea : Form {
+        public gps_nmea() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            // On form load, get a list of the open COMM ports
+
+
         }
     }
 }
